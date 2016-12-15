a76ae10b70584edb6cc59451ef7f113592c840c9
zfengli89 <249153500@qq.com>
2016-12-15 20:26:57 +0800

    test

--- engines/template-engine/src/main/test/sugo/io/pio/engine/template/TemplateEngineTest.java
+++ engines/template-engine/src/main/test/sugo/io/pio/engine/template/TemplateEngineTest.java
@@ -3,7 +3,11 @@ package sugo.io.pio.engine.template;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.Test;
+import sugo.io.pio.engine.Algorithm;
 import sugo.io.pio.engine.DataSource;
+import sugo.io.pio.engine.Preparator;
+import sugo.io.pio.engine.template.data.TemplateModelData;
+import sugo.io.pio.engine.template.data.TemplatePreparedData;
 import sugo.io.pio.engine.template.data.TemplateTrainingData;
 import sugo.io.pio.engine.template.data.input.MovielenBatchEventHose;
 
@@ -17,13 +21,18 @@ public class TemplateEngineTest {
         sparkConf.setAppName("template-engine");
 
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
-        MovielenBatchEventHose movielenBatchEventHose = new MovielenBatchEventHose("resources/movielen100k/u.data", "\t");
+        MovielenBatchEventHose movielenBatchEventHose = new MovielenBatchEventHose("resources/movielen100k/sample_movielens_data.txt", "::");
 
         TemplateEngineFactory factory = new TemplateEngineFactory();
         DataSource<TemplateTrainingData> datasource =  factory.createDatasource();
         TemplateTrainingData trainingData = datasource.readTraining(sc, movielenBatchEventHose);
 
+        Preparator<TemplateTrainingData, TemplatePreparedData> preparator = factory.createPreparator();
+        TemplatePreparedData preparedData = preparator.prepare(sc, trainingData);
 
+        Algorithm<TemplatePreparedData, TemplateModelData> modelDataAlgorithm = factory.createAlgorithm();
+        TemplateModelData modelData = modelDataAlgorithm.train(sc, preparedData);
 
+        System.out.print("ok");
     }
 }
