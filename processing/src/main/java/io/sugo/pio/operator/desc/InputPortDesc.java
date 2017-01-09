0444906e2147523d1565396ae5dfc62b1d2c79d5
janpychou <janpychou@qq.com>
2017-01-09 16:32:38 +0800

    add CustomType

--- processing/src/main/java/io/sugo/pio/operator/desc/InputPortDesc.java
+++ processing/src/main/java/io/sugo/pio/operator/desc/InputPortDesc.java
@@ -1,31 +1,17 @@
 package io.sugo.pio.operator.desc;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
-public class InputPortDesc extends CustomParameterDesc {
-    private final static String PORT_TYPE = "InputPort";
-    @JsonProperty
-    private final String portType = PORT_TYPE;
-
-    public InputPortDesc(String name) {
-        super(name, PORT_TYPE);
+public class InputPortDesc {
+    public static CustomParameterDesc create(String name) {
+        CustomParameterDesc desc = CustomParameterDesc.create(name, "InputPort");
+        desc.addParameterDesc(ParameterDesc.createString("name", ""));
+        desc.setPortType(CustomParameterDesc.CustomType.InputPort);
+        return desc;
     }
 
-    public InputPortDesc(String name, String description, List<Description> params) {
-        super(name, description, params);
-    }
-
-    public String getPortType() {
-        return portType;
-    }
-
-    public static InputPortDesc create(String name) {
-        return new InputPortDesc(name);
-    }
-
-    public static InputPortDesc create(String name, String description, List<Description> params) {
-        return new InputPortDesc(name, description, params);
+    public static CustomParameterDesc create(String name, String defaultValue) {
+        CustomParameterDesc desc = CustomParameterDesc.create(name, "InputPort");
+        desc.setPortType(CustomParameterDesc.CustomType.InputPort);
+        desc.addParameterDesc(ParameterDesc.create("name", "", ParamType.STRING, defaultValue));
+        return desc;
     }
 }
