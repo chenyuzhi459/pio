9dac08449cb5e309297f3983e5045ce99170e27e
zfengli89 <249153500@qq.com>
2017-02-10 14:37:36 +0800

    修改TextSimilarEngine

--- engines/engine-demo/src/main/java/io/sugo/pio/engine/demo/http/TextSimilarTraining.java
+++ engines/engine-demo/src/main/java/io/sugo/pio/engine/demo/http/TextSimilarTraining.java
@@ -12,7 +12,7 @@ import io.sugo.pio.engine.textSimilar.data.TextSimilarModelData;
 import io.sugo.pio.engine.textSimilar.data.TextSimilarPreparaData;
 import io.sugo.pio.engine.textSimilar.data.TextSimilarTrainingData;
 import io.sugo.pio.engine.training.*;
-import org.apache.commons.io.FileUtils;
+import io.sugo.pio.engine.demo.FileUtil;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import java.io.File;
@@ -23,19 +23,12 @@ import java.io.IOException;
 public class TextSimilarTraining extends AbstractTraining{
     @Override
     protected void doTrain(JavaSparkContext sc) throws IOException {
-
-        FileUtils.deleteDirectory(new File(TextSimilarResource.REPOSITORY_PATH));
+        FileUtil.deleteDirectory(new File(TextSimilarResource.REPOSITORY_PATH));
         BatchEventHose eventHose = new HtcBatchEventHose(Constants.HTC_DATA_PATH, Constants.HTC_DATA_SEPERATOR);
         PropertyHose propHose = new MoviePropertyHose(Constants.ITEM_PATH, Constants.ITEM_SEPERATOR, Constants.ITEM_GENS);
         Repository repository = new LocalFileRepository(TextSimilarResource.REPOSITORY_PATH);
         EngineFactory<TextSimilarTrainingData, TextSimilarPreparaData, TextSimilarModelData> engineFactory = new TextSimilarEngineFactory(propHose, eventHose, repository);
-        Preparator<TextSimilarTrainingData, TextSimilarPreparaData> preparator = engineFactory.createPreparator();
-        DataSource<TextSimilarTrainingData> dataSource = engineFactory.createDatasource();
-        TextSimilarTrainingData trainingData = dataSource.readTraining(sc);
-        TextSimilarPreparaData preparedData = preparator.prepare(sc, trainingData);
-        Algorithm<TextSimilarPreparaData, TextSimilarModelData> alg = engineFactory.createAlgorithm();
-        TextSimilarModelData modelData = alg.train(sc, preparedData);
-        Model<TextSimilarModelData> model = engineFactory.createModel();
-        model.save(modelData);
+        Engine engine = engineFactory.createEngine();
+        engine.train(sc);
     }
 }
---
+++ engines/textSimilar-engine/src/main/java/io/sugo/pio/engine/textSimilar/TextSimilarEngine.java
@@ -0,0 +1,49 @@
+package io.sugo.pio.engine.textSimilar;
+
+import io.sugo.pio.engine.data.input.BatchEventHose;
+import io.sugo.pio.engine.data.input.PropertyHose;
+import io.sugo.pio.engine.data.output.Repository;
+import io.sugo.pio.engine.textSimilar.data.TextSimilarModelData;
+import io.sugo.pio.engine.textSimilar.data.TextSimilarPreparaData;
+import io.sugo.pio.engine.textSimilar.data.TextSimilarTrainingData;
+import io.sugo.pio.engine.textSimilar.engine.TextSimilarAlgorithm;
+import io.sugo.pio.engine.textSimilar.engine.TextSimilarDatasource;
+import io.sugo.pio.engine.textSimilar.engine.TextSimilarModel;
+import io.sugo.pio.engine.textSimilar.engine.TextSimilarPreparator;
+import io.sugo.pio.engine.training.*;
+
+/**
+ */
+public class TextSimilarEngine extends Engine<TextSimilarTrainingData, TextSimilarPreparaData, TextSimilarModelData>{
+    private final BatchEventHose batchEventHose;
+    private final PropertyHose propertyHose;
+    private final Repository repository;
+
+    public TextSimilarEngine(PropertyHose propertyHose,
+                                    BatchEventHose batchEventHose,
+                                    Repository repository) {
+        this.batchEventHose = batchEventHose;
+        this.propertyHose = propertyHose;
+        this.repository = repository;
+    }
+
+    @Override
+    protected DataSource<TextSimilarTrainingData> createDatasource() {
+        return new TextSimilarDatasource(propertyHose, batchEventHose);
+    }
+
+    @Override
+    protected Preparator<TextSimilarTrainingData, TextSimilarPreparaData> createPreparator() {
+        return new TextSimilarPreparator();
+    }
+
+    @Override
+    protected Algorithm<TextSimilarPreparaData, TextSimilarModelData> createAlgorithm() {
+        return new TextSimilarAlgorithm();
+    }
+
+    @Override
+    protected Model<TextSimilarModelData> createModel() {
+        return new TextSimilarModel(repository);
+    }
+}
