c91d6d044ac93a0b92fe9702942eafd84b188d35
yaochitc <yaochi@sugo.io>
2017-02-12 19:43:50 +0800

    添加als engine调用的json例子

--- processing-engine-common/src/main/java/io/sugo/pio/engine/data/input/BatchEventHose.java
+++ processing-engine-common/src/main/java/io/sugo/pio/engine/data/input/BatchEventHose.java
@@ -1,5 +1,6 @@
 package io.sugo.pio.engine.data.input;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -7,6 +8,9 @@ import org.apache.spark.api.java.JavaSparkContext;
 /**
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes(value = {
+        @JsonSubTypes.Type(name = "mapping", value = MappingEventHose.class),
+})
 public interface BatchEventHose {
     JavaRDD<Event> find(JavaSparkContext sc);
 
