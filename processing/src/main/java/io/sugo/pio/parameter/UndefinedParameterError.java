04d888891f6160c63c947833bb92241d0ee0a8ab
Huangama <huangama.rr@qq.com>
2017-03-02 21:30:02 +0800

    Add parsing error code.

--- processing/src/main/java/io/sugo/pio/parameter/UndefinedParameterError.java
+++ processing/src/main/java/io/sugo/pio/parameter/UndefinedParameterError.java
@@ -11,31 +11,33 @@ import io.sugo.pio.operator.error.ParameterError;
  */
 public class UndefinedParameterError extends ParameterError {
 
-	private static final long serialVersionUID = -2861031839668411515L;
+    private static final long serialVersionUID = -2861031839668411515L;
 
-	/** Creates a new UndefinedParameterError. */
-	public UndefinedParameterError(String key) {
-		super(null, "pio.error.parameter_missing_1", key, "");
-	}
+    /**
+     * Creates a new UndefinedParameterError.
+     */
+    public UndefinedParameterError(String key) {
+        super(null, "pio.error.parameter_missing_1", key, "");
+    }
 
-	public UndefinedParameterError(String key, String additionalMessage) {
-		super(null, "pio.error.parameter_missing_1", key, additionalMessage);
-	}
+    public UndefinedParameterError(String key, String additionalMessage) {
+        super(null, "pio.error.parameter_missing_1", key, additionalMessage);
+    }
 
-	public UndefinedParameterError(String key, Operator operator) {
-		super(operator, "pio.error.parameter_missing_2", key, "");
-	}
+    public UndefinedParameterError(String key, Operator operator) {
+        super(operator, "pio.error.parameter_missing_2", key, "");
+    }
 
-	public UndefinedParameterError(String key, Operator operator, String additionalMessage) {
-		super(operator, "pio.error.parameter_missing_2", key, additionalMessage);
-	}
+    public UndefinedParameterError(String key, Operator operator, String additionalMessage) {
+        super(operator, "pio.error.parameter_missing_2", key, additionalMessage);
+    }
 
-	public UndefinedParameterError(Operator operator, String code, String additionalText) {
-		super(operator, code, additionalText);
-	}
+    public UndefinedParameterError(Operator operator, String code, String additionalText) {
+        super(operator, code, additionalText);
+    }
 
-	public UndefinedParameterError(Operator operator, String errorId, String parameterkey, String additionalText) {
-		super(operator, errorId, parameterkey, new Object[] { parameterkey, additionalText });
-	}
+    public UndefinedParameterError(Operator operator, String errorId, String parameterkey, String additionalText) {
+        super(operator, errorId, parameterkey, new Object[]{parameterkey, additionalText});
+    }
 
 }
