0c344b4c3cb55600043083fc24d84befefb16cf5
Huangama <huangama.rr@qq.com>
2017-03-15 17:22:45 +0800

    Convert PortType to i18n.

--- processing/src/main/java/io/sugo/pio/ports/PortType.java
+++ processing/src/main/java/io/sugo/pio/ports/PortType.java
@@ -1,37 +1,39 @@
 package io.sugo.pio.ports;
 
+import io.sugo.pio.i18n.I18N;
+
 /**
  */
 public enum PortType {
-    EXAMPLES("examples", "样本"),
-    EXAMPLE_SET("example set", "样本集"),
-    EXAMPLE_SET_INPUT("example set input", "样本集输入"),
-    EXAMPLE_SET_OUTPUT("example set output", "样本集输出"),
-    ORIGINAL("original", "原始样本集"),
-    TRAINING_EXAMPLES("training examples", "训练样本集"),
-    UNMATCHED_EXAMPLE_SET("unmatched example set", "未匹配的样本集"),
-    ITEM_SETS("item sets", "项集"),
-    FREQUENT_SETS("frequent sets", "频繁项集"),
-    CLUSTER_SET("clustered set", "簇集"),
-    TRAINING_SET("training set", "训练集"),
-    RULES("rules", "规则"),
-    MODEL("model", "模型"),
-    CLUSTER_MODEL("cluster model", "簇模型"),
-    PREPROCESSING_MODEL("preprocessing model", "预处理模型"),
-    MODEL_INPUT("model input", "模型输入"),
-    MODEL_OUTPUT("model output", "模型输出"),
-    ORIGINAL_MODEL_OUTPUT("original model output", "原始模型输出"),
-    LABELLED_DATA("labelled data", "标记的数据"),
-    UNLABELLED_DATA("unlabelled data", "未标记的数据"),
-    PERFORMANCE("performance", "性能"),
-    ESTIMATED_PERFORMANCE("estimated performance", "性能评估"),
-    THROUGH("through", "穿过"),
-    START("start", "开始"),
-    END("end", "结束"),
-    WEIGHTS("weights", "权重"),
-    OUTPUT("output", "输出"),
-    FILE("file", "文件"),
-    VECTOR("vector", "向量")
+    EXAMPLES("examples", I18N.getMessage("pio.PortType.examples")),
+    EXAMPLE_SET("example set", I18N.getMessage("pio.PortType.example_set")),
+    EXAMPLE_SET_INPUT("example set input", I18N.getMessage("pio.PortType.example_set_input")),
+    EXAMPLE_SET_OUTPUT("example set output", I18N.getMessage("pio.PortType.example_set_output")),
+    ORIGINAL("original", I18N.getMessage("pio.PortType.original")),
+    TRAINING_EXAMPLES("training examples", I18N.getMessage("pio.PortType.training_examples")),
+    UNMATCHED_EXAMPLE_SET("unmatched example set", I18N.getMessage("pio.PortType.unmatched_example_set")),
+    ITEM_SETS("item sets", I18N.getMessage("pio.PortType.item_sets")),
+    FREQUENT_SETS("frequent sets", I18N.getMessage("pio.PortType.frequent_sets")),
+    CLUSTER_SET("clustered set", I18N.getMessage("pio.PortType.cluster_set")),
+    TRAINING_SET("training set", I18N.getMessage("pio.PortType.training_set")),
+    RULES("rules", I18N.getMessage("pio.PortType.rules")),
+    MODEL("model", I18N.getMessage("pio.PortType.model")),
+    CLUSTER_MODEL("cluster model", I18N.getMessage("pio.PortType.cluster_model")),
+    PREPROCESSING_MODEL("preprocessing model", I18N.getMessage("pio.PortType.preprocessing_model")),
+    MODEL_INPUT("model input", I18N.getMessage("pio.PortType.model_input")),
+    MODEL_OUTPUT("model output", I18N.getMessage("pio.PortType.model_output")),
+    ORIGINAL_MODEL_OUTPUT("original model output", I18N.getMessage("pio.PortType.original_model_output")),
+    LABELLED_DATA("labelled data", I18N.getMessage("pio.PortType.labelled_data")),
+    UNLABELLED_DATA("unlabelled data", I18N.getMessage("pio.PortType.unlabelled_data")),
+    PERFORMANCE("performance", I18N.getMessage("pio.PortType.performance")),
+    ESTIMATED_PERFORMANCE("estimated performance", I18N.getMessage("pio.PortType.estimated_performance")),
+    THROUGH("through", I18N.getMessage("pio.PortType.through")),
+    START("start", I18N.getMessage("pio.PortType.start")),
+    END("end", I18N.getMessage("pio.PortType.end")),
+    WEIGHTS("weights", I18N.getMessage("pio.PortType.weights")),
+    OUTPUT("output", I18N.getMessage("pio.PortType.output")),
+    FILE("file", I18N.getMessage("pio.PortType.file")),
+    VECTOR("vector", I18N.getMessage("pio.PortType.vector"))
     ;
 
     private PortType(String name, String description) {
