75ea6cf53a8108c86575b3e7a6e64cb065be4a58
yaochitc <yaochi@sugo.io>
2017-03-15 14:57:40 +0800

    注入新增网络层

--- extensions-core/dl4j-extensions/src/main/java/io/sugo/pio/dl4j/DL4JModule.java
+++ extensions-core/dl4j-extensions/src/main/java/io/sugo/pio/dl4j/DL4JModule.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.databind.jsontype.NamedType;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Binder;
-import io.sugo.pio.dl4j.layers.DenseLayer;
-import io.sugo.pio.dl4j.layers.OutputLayer;
+import io.sugo.pio.dl4j.layers.*;
 import io.sugo.pio.dl4j.learner.SimpleNeuralNetwork;
+import io.sugo.pio.dl4j.word2vec.Word2VecLearner;
 import io.sugo.pio.initialization.PioModule;
 
 import java.util.List;
@@ -20,7 +20,11 @@ public class DL4JModule implements PioModule {
         return ImmutableList.of(new SimpleModule(DL4JModule.class.getSimpleName())
                 .registerSubtypes(
                         new NamedType(SimpleNeuralNetwork.class, "simple_neural_network"),
+                        new NamedType(Word2VecLearner.class, "word2vector"),
                         new NamedType(DenseLayer.class, "dense_layer"),
+                        new NamedType(ConvolutionalLayer.class, "convolutional_layer"),
+                        new NamedType(SubSamplingLayer.class, "subsampling_layer"),
+                        new NamedType(RBMLayer.class, "rbm_layer"),
                         new NamedType(OutputLayer.class, "output_layer")
                 ));
     }
